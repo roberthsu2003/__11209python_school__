f1f74dae4732cb7b15cb0e07b960b5dc095a0801
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-12 10:02:24 +0800

    modify

--- lesson17/index.py
+++ lesson17/index.py
@@ -1,10 +1,12 @@
 import dataSource
 import tkinter as tk
+from tkinter import ttk
 
 def main():
     window = tk.Tk()
     window.title("這是我的第一個視窗")
-    
+    label = tk.Label(window,text="Hello! Tkinter!",font=('Helvetica', '30'))
+    label.pack(padx=100,pady=50)
     window.mainloop()
     
 

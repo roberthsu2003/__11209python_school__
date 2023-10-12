c79b1f00cf2e9eb58b374e5a4263d9d39a0a188f
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-12 09:33:08 +0800

    modify

---
+++ lesson17/index.py
@@ -0,0 +1,12 @@
+import dataSource
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    window.title("這是我的第一個視窗")
+    
+    window.mainloop()
+    
+
+if __name__ == "__main__":
+    main()

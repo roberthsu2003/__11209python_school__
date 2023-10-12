0241f24228ee5dd4c3284253bc0980929b5a1b94
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-12 16:38:58 +0800

    modify

--- lesson17/index1.py
+++ lesson17/index1.py
@@ -28,17 +28,27 @@ class Window(tk.Tk):
         tk.Label(resultFrame,text="人口數:").grid(column=0,row=2,sticky='E',pady=5)
         tk.Label(resultFrame,text="土地面積:").grid(column=0,row=3,sticky='E',pady=5)
         tk.Label(resultFrame,text="人口密度:").grid(column=0,row=4,sticky='E',pady=5)
-        tk.Label(resultFrame,text="111").grid(column=1,row=0,sticky='W')
-        tk.Label(resultFrame,text="新北市中和區").grid(column=1,row=1,sticky='W')
-        tk.Label(resultFrame,text="403109").grid(column=1,row=2,sticky='W')
-        tk.Label(resultFrame,text="20.144").grid(column=1,row=3,sticky='W')
-        tk.Label(resultFrame,text="20011").grid(column=1,row=4,sticky='W')
+        self.yearVar = tk.StringVar()
+        tk.Label(resultFrame,textvariable=self.yearVar).grid(column=1,row=0,sticky='W')
+        self.cityVar = tk.StringVar()
+        tk.Label(resultFrame,textvariable=self.cityVar).grid(column=1,row=1,sticky='W')
+        self.population = tk.StringVar()
+        tk.Label(resultFrame,textvariable=self.population).grid(column=1,row=2,sticky='W')
+        self.areaVar = tk.StringVar()
+        tk.Label(resultFrame,textvariable=self.areaVar).grid(column=1,row=3,sticky='W')
+        self.densityVar = tk.StringVar()
+        tk.Label(resultFrame,textvariable=self.densityVar).grid(column=1,row=4,sticky='W')
         resultFrame.pack()
 
     def user_selected(self,event):
         selectedIndex = self.listbox.curselection()[0]
         cityName = self.listbox.get(selectedIndex)
-        print(dataSource.info(cityName))
+        datalist = dataSource.info(cityName)
+        self.yearVar.set(datalist[0])
+        self.cityVar.set(datalist[1])
+        self.population.set(datalist[2])
+        self.areaVar.set(datalist[3])
+        self.densityVar.set(datalist[4])
 
 
 def main():

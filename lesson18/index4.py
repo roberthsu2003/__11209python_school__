b87cbfe28aac8c3a79e2179b07edb0a410e6fa14
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-16 14:29:06 +0800

    modify

--- lesson18/index4.py
+++ lesson18/index4.py
@@ -10,16 +10,14 @@ class Window(tk.Tk):
         super().__init__(**kwargs)        
         self.title("Image")
         self.geometry("300x250")
-        self.configure(background='#E79460')
+        #self.configure(background='#E79460')
 
-class MyFrame(ttk.LabelFrame):
+class MyFrame(tk.LabelFrame):
     def __init__(self,master,title,**kwargs):
         super().__init__(master,text=title,**kwargs)
-        self.aligement = tk.StringVar(value='left')
-        ttk.Radiobutton(self,text="左邊",value='left',variable=self.aligement,command=self.choised).grid(column=0,row=0,padx=10)
-        ttk.Radiobutton(self,text="中間",value='center',variable=self.aligement,command=self.choised).grid(column=1,row=0,padx=10)
-        ttk.Radiobutton(self,text="右邊",value='right',variable=self.aligement,command=self.choised).grid(column=2,row=0,padx=10)
-        self.pack(expand=1, fill='x',padx=10,pady=10)
+        self.pack(expand=1,fill='both',padx=10,pady=10)
+       
+        
 
     def choised(self):
         print(self.aligement.get())
@@ -27,11 +25,7 @@ class MyFrame(ttk.LabelFrame):
 
 def main():    
     window = Window()
-    myFrame = MyFrame(window,"對齊方式")
-    s = ttk.Style()
-    print(s.theme_names())
-    print(s.theme_use())
-    #print(s.theme_use('clam'))
+    myFrame = MyFrame(window,"對齊方式")    
     window.mainloop()
 
 if __name__ == "__main__":

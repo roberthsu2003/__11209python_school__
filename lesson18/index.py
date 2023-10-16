694f1e29e5354a764eaba37a6e51636b8c768966
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-16 10:01:09 +0800

    modify

--- lesson18/index.py
+++ lesson18/index.py
@@ -9,15 +9,19 @@ class Window(tk.Tk):
         super().__init__(**kwargs)
         self.geometry("400x250+300+300")
         self.title("Lines")
+        self.configure(background='#E79460')
 
 class MyFrame(tk.Frame):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
+    def __init__(self,master,**kwargs):
+        super().__init__(master,**kwargs)
+        self.configure(background='#9E7A7A')
+        self.pack(expand=1, fill='both')
 
 
 def main():    
     window = Window()
+    myFrame = MyFrame(window)
     window.mainloop()
-    
+
 if __name__ == "__main__":
     main()

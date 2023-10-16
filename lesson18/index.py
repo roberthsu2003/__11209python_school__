ba2c806d1856116fca8429631ca8d2a6696337aa
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-16 10:37:43 +0800

    modify

--- lesson18/index.py
+++ lesson18/index.py
@@ -15,6 +15,11 @@ class MyFrame(tk.Frame):
     def __init__(self,master,**kwargs):
         super().__init__(master,**kwargs)
         self.configure(background='#9E7A7A')
+        canvas = tk.Canvas(self)
+        canvas.create_line(15, 30, 200, 30)
+        canvas.create_line(300, 35, 300, 200, dash=(4, 2))
+        canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
+        canvas.pack(expand=1,fill='both')
         self.pack(expand=1, fill='both')
 
 

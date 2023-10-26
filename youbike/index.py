fbc2d114f4237a36482f4bde6830671aa988166a
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-26 10:07:03 +0800

    modify

--- youbike/index.py
+++ youbike/index.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+import datasource
+
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        try:
+            datasource.download_youbike_data()
+        except Exception as e:
+            messagebox.showerror("錯誤",f'{e}\n將關閉應用程式\n請稍後再試')
+            self.destroy()
         
 
 def main():

59c1b9090f9fee7a91075523c30d033a6cca627f
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-02 11:59:59 +0800

    modify

--- youbike/youbikeTreeView.py
+++ youbike/youbikeTreeView.py
@@ -21,6 +21,9 @@ class YoubikeTreeView(ttk.Treeview):
         self.column('sbi',width=50)
         self.column('bemp',width=50)
 
+        #----------bind button1-------
+        self.bind('<ButtonRelease-1>', self.selectedItem)
+
     def update_content(self,site_datas):
         '''
         更新內容
@@ -29,9 +32,15 @@ class YoubikeTreeView(ttk.Treeview):
         for i in self.get_children():
             self.delete(i)
         
-        for site in site_datas:
-            self.insert('','end',values=site)
+        for index,site in enumerate(site_datas):
+            self.insert('','end',text=f"abc{index}",values=site)
 
+
+    def selectedItem(self,event):
+        selectedItem = self.focus()
+        print(selectedItem)
+        print(self.item(selectedItem))
+        
 
 
         

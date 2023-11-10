1f36112bdd8fee35dc35e8142822119aa10c4059
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-10 10:51:54 +0800

    modify

--- youbike_render/上課用.sql
+++ youbike_render/上課用.sql
@@ -15,3 +15,8 @@ ON CONFLICT (站點名稱,更新時間) DO UPDATE
 select * 
 from 台北市youbike
 where 站點名稱='YouBike2.0_捷運科技大樓站'
+
+
+INSERT INTO 台北市youbike (站點名稱, 行政區, 更新時間, 地址, 總車輛數, 可借, 可還) 
+VALUES ('YouBike2.0_一壽橋','文山區','2023-11-08 10:43:16','樟新街64號前方',0,0,0)
+ON CONFLICT (站點名稱,更新時間) DO NOTHING

d7e32876005c89a2bb978d769a9052519012289c
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-20 05:42:34 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -10,3 +10,18 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'<h1>User {username}</h1>'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'<h1>Post {post_id * 5}</h1>'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'<h1>Subpath {subpath}</h1>'

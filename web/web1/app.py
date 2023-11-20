cb2a556ae1a19e0d6f3c60df57e43fb474418a77
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-20 06:26:26 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -1,9 +1,13 @@
 from flask import Flask,url_for,render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     name = "徐國堂"
-    age = 25
+    age = random_age()
     return render_template('index.html',name=name,age=age)
+
+def random_age():
+    return random.randint(25,35)

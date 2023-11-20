382d06f39f17feadc8b868017ed8a22188756f05
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-20 06:19:48 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -4,30 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'<h1>User {username}</h1>'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'<h1>Post {post_id * 5}</h1>'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'<h1>Subpath {subpath}</h1>'
-
-@app.route('/url')
-def url():
-    print(url_for('hello'))
-    print(url_for('show_user_profile',username="RobertHsu"))
-    print(url_for('static', filename='css/style.css'))
-    return "ABC"
+    name = "徐國堂"
+    age = 25
+    return render_template('index.html',name=name,age=age)

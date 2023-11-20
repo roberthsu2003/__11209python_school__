ae885728e399132469c573ce7d51fd8a6dd30415
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-20 06:45:22 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -1,13 +1,18 @@
 from flask import Flask,url_for,render_template
 import random
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     name = "徐國堂"
-    age = random_age()
-    return render_template('index.html',name=name,age=age)
+    dataFrame = get_dataFrame()
+    return render_template('index.html',name=name,dataFrame=dataFrame)
 
-def random_age():
-    return random.randint(25,35)
+def get_dataFrame()->pd.DataFrame:
+    data = [['徐國堂',67,92,85],
+            ['王XX',78,97,65],
+            ['李XX',92, 79, 85]]    
+    
+    return pd.DataFrame(data,columns=["姓名","國文","英文","數學"])

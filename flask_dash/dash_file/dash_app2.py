85e488703e3b0cb5adee7aca7c4d88a27de87243
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-06 08:04:52 +0000

    modify

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,23 +1,13 @@
 from dash import Dash, html,dash_table
 import pandas as pd
 import dash_bootstrap_components as dbc
-from collections import OrderedDict
+from . import datasource
+import pandas as pd
 
 dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 dash2.title = "台北市youbike及時資料"
-
-data = OrderedDict(
-    [
-        ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
-        ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
-        ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
-        ("Humidity", [10, 20, 30, 40, 50, 60]),
-        ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
-    ])
-
-df = pd.DataFrame(
-    OrderedDict([(name, col_data * 10) for (name, col_data) in data.items()])
-)
+lastest_data = datasource.lastest_datetime_data()
+lastest_df = pd.DataFrame(lastest_data,columns=['站點名稱','更新時間','行政區','地址','總數','可借','可還'])
 
 dash2.layout = html.Div(
     [
@@ -32,8 +22,8 @@ dash2.layout = html.Div(
             html.Div([
                 html.Div([
                     dash_table.DataTable(
-                        data=df.to_dict('records'),
-                        columns=[{'id':column,'name':column} for column in df.columns],
+                        data=lastest_df.to_dict('records'),
+                        columns=[{'id':column,'name':column} for column in lastest_df.columns],
                         page_size=20
                     ),
                 ],className="col text-center")

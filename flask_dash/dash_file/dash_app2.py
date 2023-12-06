59a143124caa2c17471b9ad7b8fb924e0148613c
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-06 06:34:23 +0000

    modify

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,9 +1,15 @@
-from dash import Dash, html
+from dash import Dash, html,dash_table
 import pandas as pd
 import dash_bootstrap_components as dbc
 
 dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 dash2.title = "台北市youbike及時資料"
+
+df = pd.DataFrame({
+    "水果": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "數量": [4, 1, 2, 2, 4, 5],
+    "城市": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
 dash2.layout = html.Div(
     [
@@ -11,6 +17,13 @@ dash2.layout = html.Div(
             html.Div([
                 html.Div([
                     html.H1("台北市youbike及時資料")
+                ],className="col text-center")
+            ],
+            className="row",
+            style={"paddingTop":'2rem'}),
+            html.Div([
+                html.Div([
+                    dash_table.DataTable(df.to_dict('records')),
                 ],className="col text-center")
             ],
             className="row",

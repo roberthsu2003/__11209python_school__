94180dfbfb52154bc1b871ba6a287ada2a1424ce
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-06 03:31:59 +0000

    modify

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,9 +1,15 @@
 from dash import Dash, html
 import pandas as pd
-dash2 = Dash(requests_pathname_prefix="/dash/app2/")
+import dash_bootstrap_components as dbc
 
-dash2.layout = html.Div([
-    html.H1("Dash H1"),
-    html.P("這是段落1"),
-    html.P("這是段落2")
-])
+dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+dash2.layout = html.Div(
+    [html.H1("BootStrap Layout"),
+     html.P("這是段落1"),
+     html.P("這是段落2")
+    ],
+    className="container-lg",
+    style={'backgroundColor':'#666'}
+
+    )

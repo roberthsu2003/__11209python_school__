a8cb96b03159f81ed9785f471299decb341f1a7a
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-19 03:07:46 +0000

    modify

--- flask_dash/auth/auth.py
+++ flask_dash/auth/auth.py
@@ -1,8 +1,13 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 
 blueprint_auth = Blueprint('auth', __name__,url_prefix='/auth')
 
-@blueprint_auth.route('/')
-@blueprint_auth.route('/login')
+@blueprint_auth.route('/',methods=['GET', 'POST'])
+@blueprint_auth.route('/login',methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        print(request.form['email'])
+        print(request.form['password'])
+    
+
     return render_template("/auth/login.html")

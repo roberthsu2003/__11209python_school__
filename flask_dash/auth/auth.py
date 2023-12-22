38ac2c6757a8a3c4a51be970d3ecc842c8678ca1
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-22 01:30:03 +0000

    modify

--- flask_dash/auth/auth.py
+++ flask_dash/auth/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,request,redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField,SelectField,EmailField,BooleanField,DateField
+from wtforms import StringField,SelectField,EmailField,BooleanField,DateField,TextAreaField
 from wtforms.validators import DataRequired,Length,Regexp,Optional
 
 blueprint_auth = Blueprint('auth', __name__,url_prefix='/auth')
@@ -33,6 +33,7 @@ class UserRegistrationForm(FlaskForm):
     uEmail = EmailField("電子郵件",validators=[DataRequired()])
     isGetEmail = BooleanField("接受促銷email",default=False)
     uBirthday = DateField("出生年月日",validators=[Optional()],format='%Y-%m-%d')
+    uAboutMe = TextAreaField('自我介紹', [Optional(), Length(max=200)])
 
 
 @blueprint_auth.route('/registor',methods=['GET','POST'])
@@ -58,6 +59,9 @@ def register():
 
             uBirthday = form.uBirthday.data
             print("出生",uBirthday)
+
+            uAboutMe = form.uAboutMe.data
+            print("自我介紹",uAboutMe)
             
         else:
             print("驗證失敗")
